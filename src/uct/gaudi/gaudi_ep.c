81bff92b6016588070537428aac1a5937aee8f1f
Henry Tang <ytang@habana.ai>
2025-06-14 16:38:34 +0000

    update unittest

--- src/uct/gaudi/gaudi_ep.c
+++ src/uct/gaudi/gaudi_ep.c
@@ -46,3 +46,57 @@ ucs_status_t uct_gaudi_ep_flush(uct_ep_h ep, unsigned flags, const uct_completio
     ucs_trace("Fake Gaudi flush");
     return UCS_OK;
 }
+
+ucs_status_t uct_gaudi_ep_get_short(uct_ep_h ep, void *buffer, unsigned length,
+                                    uint64_t remote_addr, uct_rkey_t rkey, void *comp)
+{
+    ucs_trace("Fake Gaudi get_short: len=%u remote_addr=0x%lx", length, remote_addr);
+    return UCS_ERR_UNSUPPORTED;
+}
+
+ssize_t uct_gaudi_ep_put_bcopy(uct_ep_h ep, uct_pack_callback_t pack_cb,
+                               void *arg, uint64_t remote_addr, uct_rkey_t rkey)
+{
+    ucs_trace("Fake Gaudi put_bcopy: remote_addr=0x%lx", remote_addr);
+    return UCS_ERR_UNSUPPORTED;
+}
+
+ssize_t uct_gaudi_ep_get_bcopy(uct_ep_h ep, uct_unpack_callback_t unpack_cb,
+                               void *arg, uint64_t remote_addr, uct_rkey_t rkey, void *comp)
+{
+    ucs_trace("Fake Gaudi get_bcopy: remote_addr=0x%lx", remote_addr);
+    return UCS_ERR_UNSUPPORTED;
+}
+
+ucs_status_t uct_gaudi_ep_am_short(uct_ep_h ep, uint8_t id, uint64_t header,
+                                   const void *payload, unsigned length)
+{
+    ucs_trace("Fake Gaudi am_short: id=%u len=%u", id, length);
+    return UCS_ERR_UNSUPPORTED;
+}
+
+ssize_t uct_gaudi_ep_am_bcopy(uct_ep_h ep, uint8_t id, uct_pack_callback_t pack_cb,
+                              void *arg, unsigned flags)
+{
+    ucs_trace("Fake Gaudi am_bcopy: id=%u", id);
+    return UCS_ERR_UNSUPPORTED;
+}
+
+ucs_status_t uct_gaudi_ep_am_zcopy(uct_ep_h ep, uint8_t id, const void *header,
+                                   unsigned header_length, const uct_iov_t *iov,
+                                   size_t iovcnt, unsigned flags, uct_completion_t *comp)
+{
+    ucs_trace("Fake Gaudi am_zcopy: id=%u iovcnt=%zu", id, iovcnt);
+    return UCS_ERR_UNSUPPORTED;
+}
+
+ucs_status_t uct_gaudi_ep_pending_add(uct_ep_h ep, uct_pending_req_t *req, unsigned flags)
+{
+    ucs_trace("Fake Gaudi pending_add");
+    return UCS_ERR_UNSUPPORTED;
+}
+
+void uct_gaudi_ep_pending_purge(uct_ep_h ep, uct_pending_purge_callback_t cb, void *arg)
+{
+    ucs_trace("Fake Gaudi pending_purge");
+}
--- unitest.sh
+++ unitest.sh
@@ -3,6 +3,12 @@
 # Usage: ./unitest.sh [gtest_filter]
 set -e
 
+# Set Habana Labs library and include paths
+export LD_LIBRARY_PATH=/usr/lib/habanalabs:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/usr/lib/habanalabs:$LIBRARY_PATH
+export CFLAGS="-I/usr/include/habanalabs -I/opt/habanalabs/src/hl-thunk/include -I/opt/habanalabs/src/hl-thunk/include/uapi $CFLAGS"
+export LDFLAGS="-L/usr/lib/habanalabs $LDFLAGS"
+
 # Build UCX with Gaudi and GTest support
 echo "[INFO] Running autogen.sh and configure..."
 ./autogen.sh
