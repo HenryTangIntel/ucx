4ad225b14f16039d97755377749baefe726a646c
User <user@example.com>
2025-06-25 01:21:30 +0000

    new compile ok

--- test/gtest/ucm/gaudi/test_gaudi.cc
+++ test/gtest/ucm/gaudi/test_gaudi.cc
@@ -1,83 +1,14 @@
-/**
- * Copyright (C) 2023, Habana Labs. ALL RIGHTS RESERVED.
- * See file LICENSE for terms.
- */
-
-#include <common/test.h>
-#include <ucm/api/ucm.h>
-#include <ucm/gaudi/gaudimem.h>
-
-class test_ucm_gaudi : public ucs::test {
-protected:
-    virtual void init() {
-        ucs::test::init();
-    }
-
-    virtual void cleanup() {
-        ucs::test::cleanup();
-    }
-};
-
-UCS_TEST_F(test_ucm_gaudi, init_cleanup) {
-    ucs_status_t status;
-
-    /* Test initialization */
-    status = ucm_gaudi_mem_init();
 #ifdef HAVE_GAUDI
-    EXPECT_EQ(UCS_OK, status);
-#else
-    EXPECT_EQ(UCS_ERR_UNSUPPORTED, status);
-#endif
+UCS_TEST_F(test_ucm_gaudi, hlthunk_allocate_and_free_device_memory) {
+    int device_id = 0; // Use device 0 for testing
+    void *dptr = NULL;
+    size_t size = 4096;
 
-    /* Test cleanup - should not crash or cause errors */
-    ucm_gaudi_mem_cleanup();
+    int ret = ucm_hlthunk_allocate_device_memory(device_id, &dptr, size);
+    EXPECT_EQ(ret, 0);
+    EXPECT_NE(dptr, nullptr);
+
+    ret = ucm_hlthunk_free_device_memory(device_id, dptr);
+    EXPECT_EQ(ret, 0);
 }
-
-UCS_TEST_F(test_ucm_gaudi, double_init) {
-    ucs_status_t status;
-    
-    /* First init */
-    status = ucm_gaudi_mem_init();
-#ifdef HAVE_GAUDI
-    EXPECT_EQ(UCS_OK, status);
-#else
-    EXPECT_EQ(UCS_ERR_UNSUPPORTED, status);
 #endif
-
-    /* Second init - should behave the same */
-    status = ucm_gaudi_mem_init();
-#ifdef HAVE_GAUDI
-    EXPECT_EQ(UCS_OK, status);
-#else
-    EXPECT_EQ(UCS_ERR_UNSUPPORTED, status);
-#endif
-
-    /* Cleanup */
-    ucm_gaudi_mem_cleanup();
-}
-
-UCS_TEST_F(test_ucm_gaudi, init_cleanup_cycle) {
-    ucs_status_t status;
-    
-    for (int i = 0; i < 5; ++i) {
-        status = ucm_gaudi_mem_init();
-#ifdef HAVE_GAUDI
-        EXPECT_EQ(UCS_OK, status);
-#else
-        EXPECT_EQ(UCS_ERR_UNSUPPORTED, status);
-#endif
-
-        ucm_gaudi_mem_cleanup();
-    }
-}
-
-UCS_TEST_F(test_ucm_gaudi, unsupported_on_non_gaudi) {
-#ifndef HAVE_GAUDI
-    ucs_status_t status;
-    
-    status = ucm_gaudi_mem_init();
-    EXPECT_EQ(UCS_ERR_UNSUPPORTED, status);
-    
-    ucm_gaudi_mem_cleanup(); /* Should be safe to call even if init failed */
-#endif
-}
